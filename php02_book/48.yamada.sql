811d70cd648b8b93c59ece5991649a72849b0752
YMD-48 <69379431+YMD-48@users.noreply.github.com>
2020-11-09 15:30:39 +0900

    Add files via upload

---
+++ php02_book/48.yamada.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost:8889
+-- 生成日時: 
+-- サーバのバージョン： 5.7.24
+-- PHP のバージョン: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gs_db2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_bm_table`
+--
+
+CREATE TABLE `gs_bm_table` (
+  `id` int(12) NOT NULL,
+  `bookname` varchar(64) NOT NULL,
+  `bookurl` text NOT NULL,
+  `bookcomment` text NOT NULL,
+  `bookdate` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `gs_bm_table`
+--
+
+INSERT INTO `gs_bm_table` (`id`, `bookname`, `bookurl`, `bookcomment`, `bookdate`) VALUES
+(1, 'テスト1', 'https://zxc', 'c v', '2020-11-09 15:16:30'),
+(2, 'aaaaaaaaa', 'https://aaabbbb', 'cccccccc', '2020-11-09 15:17:34'),
+(3, 'asdfgh', 'https://sdf', 'asdfgh', '2020-11-09 15:18:28');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルのAUTO_INCREMENT
+--
+
+--
+-- テーブルのAUTO_INCREMENT `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
